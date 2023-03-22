e8bdb456b33d4cc12b477413411c362caa4c8f7a
wangjianjun <>
2023-03-22 10:56:55 +0800

    提交 ijkplayer k0.8.8 源码，未做任何修改

---
+++ ijkplayer-k0.8.8/ios/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLHudViewController.h
@@ -0,0 +1,18 @@
+//
+//  IJKSDLHudViewController.h
+//  IJKMediaPlayer
+//
+//  Created by Zhang Rui on 15/12/14.
+//  Copyright © 2015年 bilibili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJKSDLHudViewController : UITableViewController
+
+- (id)init;
+
+- (void)setHudValue:(NSString *)value forKey:(NSString *)key;
+
+- (void)setRect:(CGRect) rect;
+@end
